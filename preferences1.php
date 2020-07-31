7efc9cd10ff509b55b74d97cce7e51a08b17a16c
Alan Navarro <alan@localhost.localdomain>
2020-07-30 19:21:56 -0600

    Prueba3

--- preferences1.php
+++ preferences1.php
@@ -3,16 +3,22 @@
 require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('TEST-8299436681826487-073017-5e6ace7a2626a84d152b9457');
+MercadoPago\SDK::setAccessToken('PP_USR-8299436681826487-073017-9384acc9459b0c1404d77d6360b7342e-293829152');
 
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
 // Crea un ítem en la preferencia
 $item = new MercadoPago\Item();
-$item->title = 'Mi producto';
-$item->quantity = 1;
+$item->title = 'GALAXY S9';
+$item->quantity = 10;
 $item->unit_price = 15;
 $preference->items = array($item);
+$item = new MercadoPago\Item();
+$item->title = 'LG CG';
+$item->quantity = 10;
+$item->unit_price = 10;
+$preference->items = array($item);
+
 $preference->save();
 ?>
